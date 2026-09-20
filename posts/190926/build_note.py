e2f33ffe4433e4f4b95a0b6a8793151e1e2308c8
Adam Mendoza <work.admmndz@gmail.com>
2026-09-20 20:49:50 +0800

    fixed header

--- posts/190926/build_note.py
+++ posts/190926/build_note.py
@@ -23,8 +23,11 @@ html = html.replace("""MathJax.Hub.Config({
 
 header_css = """
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;500;700&display=swap');
+
 html {
   box-sizing: border-box;
+  scroll-behavior: smooth;
 }
 
 *, *:before, *:after {
@@ -55,6 +58,7 @@ body.jp-Notebook {
   display: flex;
   align-items: center;
   justify-content: space-between;
+  font-family: "Noto Sans", "Helvetica Neue", Helvetica, Arial, sans-serif !important;
 }
 
 .nav-name {
@@ -81,6 +85,7 @@ body.jp-Notebook {
   display: flex;
   align-items: center;
   gap: 42px;
+  font-family: "Noto Sans", "Helvetica Neue", Helvetica, Arial, sans-serif !important;
 }
 
 .nav-links a {
@@ -492,7 +497,11 @@ document.addEventListener("DOMContentLoaded",()=>{const tocList=document.getElem
 
 html = html.replace("</head>", header_css + "</head>", 1)
 
-html = html.replace("<main>", '<div class="note-layout">' + toc_html + "<main>", 1)
+html = html.replace(
+    "<main>",
+    '<div class="note-layout">' + toc_html + "<main>",
+    1
+)
 
 html = html.replace("</main>", "</main></div>", 1)
 
